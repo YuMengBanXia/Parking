ee99bf180aad73396e98ab821523d5ba2a024c5e
Da <daye@ucm.es>
2025-04-02 18:54:19 +0200

    ticket cogido

--- ticketCogido.php
+++ ticketCogido.php
@@ -8,19 +8,30 @@ require_once __DIR__ . '/includes/config.php';
 $tituloPagina = 'Ticket cogido';
 
 
-$formTicketCogido=new \es\ucm\fdi\aw\ePark\cogidoTicket();
+$ticket = $_GET['ticket'] ?? null;
+$matricula = $_GET['matricula'] ?? null;
+$fecha = $_GET['fecha'] ?? null;
+
+$html='';
+if($ticket!=null && $matricula!=null && $fecha!=null){
+$html =<<<EOF
+   ID del ticket: $ticket <br>
+   Matrícula: $matricula<br>
+   Fecha entrada: $fecha <br>
+      <a href="index.php">
+         <button type="button">Ir al inicio</button>
+   </a>
+EOF;
+}
 
 
 $htmlFormCogido=$formTicketCogido->Manage();
 
 $contenidoPrincipal = <<<EOS
    <h3>DATOS DEL TICKET</h3>
-   $htmlFormCogido
+   $html
 
 EOS;
-
-
-
 
 require_once __DIR__ .'/includes/vistas/plantilla/plantilla.php';
 
