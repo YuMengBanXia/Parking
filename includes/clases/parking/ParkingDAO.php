624bf11ed72dabad2be4668ffb437c2e7ddfe774
Chloe221201 <mengxiazhou@gmail.com>
2025-04-06 01:01:33 +0200

    borrar usuarios insertados

--- includes/clases/parking/ParkingDAO.php
+++ includes/clases/parking/ParkingDAO.php
@@ -68,6 +68,7 @@ class ParkingDAO extends DAO {
             { 
                 throw new parkingAlreadyExistsException("Ya existe el parking en la dirección '{$p->getDir()}' y ciudad {$p->getCiudad()}");
             }
+            
             throw $e;
         }
         return $result;
--- includes/mysql/03_datosTablas.sql
+++ includes/mysql/03_datosTablas.sql
@@ -7,10 +7,6 @@ DELETE FROM `Plaza`;
 DELETE FROM `Parking`;
 DELETE FROM `Usuario`;
 
-INSERT INTO `Usuario` (`dni`, `nomUsuario`, `contrasenia`, `tipoUsuario`) VALUES 
-('12345678A', 'nombreEjemplo', 'contraseñaEjemplo', 'cliente'),
-('12345678B', 'nombreEjemplo2', 'contraseñaEjemplo2', 'propietario'),
-('12345678C', 'nombreEjemplo3', 'contraseñaEjemplo3', 'administrador');
 
 INSERT INTO `Parking` (`id`, `dni`, `dir`, `ciudad`, `CP`, `precio`, `nPlazas`) VALUES
 (1, '12345678A', 'Calle Princesa', 'Madrid', 28008, 1.23, 3),
