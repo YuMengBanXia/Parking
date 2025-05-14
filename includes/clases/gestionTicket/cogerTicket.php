d4f81252fec555f8ae5fcd834607481fdd835aa1
ZJie <javierzh@ucm.es>
2025-05-14 17:53:46 +0200

    V1Geolocalizacion

--- includes/clases/gestionTicket/cogerTicket.php
+++ includes/clases/gestionTicket/cogerTicket.php
@@ -4,10 +4,8 @@ namespace es\ucm\fdi\aw\ePark;
 
 class cogerTicket extends formBase
 {
-
     public function __construct()
     {
-
         parent::__construct('cogerTicket');
     }
 
@@ -17,13 +15,12 @@ class cogerTicket extends formBase
         $idSeleccionado = $datos['parking_id'] ?? '';
         $parkings = SAParking::mostrarParkings();
 
-
         $html = <<<EOF
         <label for="matricula">Matrícula:</label>
         <input type="text" id="matricula" name="matricula" required pattern="\d{4}[A-Za-z]{3}" 
         title="Introduce una matrícula válida: 4 dígitos seguidos de 3 letras (ej. 1234ABC)" value="{$matricula}">
         <div class="tabla-responsive">
-        <table id=tablaParkings>
+        <table id="tablaParkings">
             <thead><tr>
                 <th>ID</th>
                 <th>Imagen</th>
@@ -31,6 +28,7 @@ class cogerTicket extends formBase
                 <th>Ciudad</th>
                 <th>Precio €</th>
                 <th>Plazas Disponibles</th>
+                <th>Distancia</th>
                 <th>Escoger</th>
             </thead></tr>
         EOF;
@@ -41,29 +39,26 @@ class cogerTicket extends formBase
             $ocupadas = htmlspecialchars(SAParking::ocupacion($id));
             $libres = $nPlazas - $ocupadas;
 
-
-            //Si el parking no tiene plazas libres lo saltamos
-            if($libres <= 0) {
-                continue;
-            }
+            if($libres <= 0) continue;
 
             $dir = htmlspecialchars($parking->getDir());
             $ciudad = htmlspecialchars($parking->getCiudad());
             $precio = htmlspecialchars($parking->getPrecio());
+            $direccionCompleta = "$dir, $ciudad";
 
             $checked = ($idSeleccionado == $id) ? 'checked' : '';
-
             $imgPath = (!empty($parking->getImg()) && file_exists($parking->getImg())) ? $parking->getImg() : "img/default.png";
             $img = htmlspecialchars($imgPath);
 
             $html .= <<<EOF
-            <tr>
+            <tr data-direccion="{$direccionCompleta}">
                 <td>{$id}</td>
-                <td><img src="{$img}" alt="Imagen del parking"></td>
+                <td><img src="{$img}" alt="Imagen del parking" style="max-width: 100px;"></td>
                 <td>{$dir}</td>
                 <td>{$ciudad}</td>
                 <td>{$precio}</td>
                 <td>{$libres}</td>
+                <td class="distancia">Calculando...</td>
                 <td>
                     <label><input type="radio" name="parking_id" value="{$id}" {$checked}></label>
                 </td>
@@ -71,17 +66,20 @@ class cogerTicket extends formBase
             EOF;
         }
 
-        $html .= "</table>";
-        $html .="</div>";
-        $html .= '<button type="submit">Confirmar</button>';
-        $htmlinicio = <<<EOF
-            <a href="index.php" class="btn-link">Ir al inicio</a>
+        $html .= "</table></div>";
+        $html .= '<button type="submit" class="btn-confirmar">Confirmar</button>';
+        $html .= '<a href="index.php" class="btn-link">Ir al inicio</a>';
+
+        // Inclusión del archivo JS externo con atribución a OpenStreetMap
+        $html .= <<<EOF
+        <script src=RUTA_JS . "calculoDistancias.js" defer></script>
+        <footer class="osm-attribution">
+            © <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>
+        </footer>
         EOF;
 
-
-        return $html .= $htmlinicio;
+        return $html;
     }
-
 
     protected function Process($datos)
     {
@@ -104,12 +102,9 @@ class cogerTicket extends formBase
         if (count($result) === 0) {
             $respuesta = SATicket::nuevoTicket($id, $matricula);
             if (is_array($respuesta)) {
-                // Éxito: el código es $resultado[0] (que será 4) 
-                // y $resultado[1] contiene los datos del ticket.
                 $num = $respuesta[0];
                 $datos = $respuesta[1];
             } else {
-                // Error: $resultado es numérico (0,1,2,3) y representa el código de error.
                 $num = $respuesta;
             }
 
@@ -117,16 +112,16 @@ class cogerTicket extends formBase
                 case 0:
                     $result[] = "Faltan datos por seleccionar";
                     break;
-                case 1: //Errores con el dato id
+                case 1:
                     $result[] = 'Algo ha salido mal, por favor vuelva a seleccionar un parking disponible';
                     break;
-                case 2: //Matrícula ya encontrada dentro de un parking
+                case 2:
                     $result[] = 'La matrícula introducida ha sido encontrada en otro parking';
                     break;
-                case 3: //Error base de datos
+                case 3:
                     $result[] = 'Ha habido un error en la base de datos';
                     break;
-                case 4: //Éxito
+                case 4:
                     $codigo = $datos['codigo'];
                     $fecha = $datos['fecha']->format('d-m-Y H:i:s');
 
@@ -137,9 +132,8 @@ class cogerTicket extends formBase
                     ]);
 
                     $result = 'ticketCogido.php?' . $params;
-
                     break;
-                default: //caso 0 (faltan datos) o error inesperado
+                default:
                     $result[] = 'Ha habido un error inesperado vuelva a intentarlo';
                     break;
             }
