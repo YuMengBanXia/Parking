4ef372ad38c58c99413f3b4978521cba2e8a3fd2
Chloe221201 <mengxiazhou@gmail.com>
2025-05-14 16:23:29 +0200

    errata creación administrador

--- includes/mysql/01_createUser.sql
+++ includes/mysql/01_createUser.sql
@@ -1,6 +1,6 @@
 -- Creación del administrador de la base de datos
 CREATE USER 'adminDB'@'localhost' IDENTIFIED BY 'adminDBPassword';
-GRANT ALL PRIVILEGES ON `ePArk` . * TO 'adminDB'@'localhost';
+GRANT ALL PRIVILEGES ON `ePark` . * TO 'adminDB'@'localhost';
 
 CREATE USER 'adminDB'@'127.0.0.1' IDENTIFIED BY 'adminDBPassword';
-GRANT ALL PRIVILEGES ON `ePArk` . * TO 'adminDB'@'127.0.0.1';
+GRANT ALL PRIVILEGES ON `ePark` . * TO 'adminDB'@'127.0.0.1';
